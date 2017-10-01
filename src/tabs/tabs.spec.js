f90feb455aa20846aa6f3cb2e227b7ee84683942
James Friedman <jamesmfriedman@gmail.com>
2017-09-30 23:02:47 -0400

    running build

--- src/tabs/tabs.spec.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { TabBar, Tab } from './tabs';
-
-describe('Tabs', () => {
-  it('renders', () => {
-    mount(<TabBar activeTabIndex={0} onChange={evt => {}} />);
-  });
-});
